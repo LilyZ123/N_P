1d5f5185c1ef9ae0cb67736cb84c6d331a7ab498
tjnhzlb@gmail.com <zhang.lib>
2024-02-08 19:12:25 -0500

    test2

---
+++ input.sol
@@ -0,0 +1,13 @@
+Soil Series Name: NOAHMP_Soil
+Hydrologic Group: B
+Maximum Rooting Depth (mm): 1500
+Anion Exclusion Fraction: 0.8
+Crack Volume Potential: 0.07
+
+Layer   Depth (mm)   Bulk Density (Mg/m^3)   Available Water Capacity (mm H2O/mm soil)   Saturated Hydraulic Conductivity (mm/hr)   Percent Organic Carbon (%)   Percent Clay (%)   Percent Silt (%)   Percent Sand (%)   Percent Rock Fragments (%)   Albedo   Electrical Conductivity (dS/m)   Soil pH   Calcium Carbonate Content (%)
+1       0            1.25                    0.30                                       15                                     2.0                           25               35               40               5                          0.2     0.6                        6.0      2.5
+2       200          1.30                    0.25                                       12                                     1.8                           22               38               40               5                          0.2     0.6                        6.0      2.5
+3       400          1.35                    0.22                                       10                                     1.6                           20               40               40               5                          0.2     0.6                        6.0      2.5
+4       700          1.40                    0.18                                       8                                      1.4                           18               42               40               5                          0.2     0.6                        6.0      2.5
+5       1200         1.45                    0.15                                       6                                      1.2                           16               44               40               5                          0.2     0.6                        6.0      2.5
+
